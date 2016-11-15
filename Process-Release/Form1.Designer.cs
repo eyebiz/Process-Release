efe85f953c2fd74a5c7fa155c92b0b61a143e372
eyebiz <eyebiz@gmail.com>
2016-11-15 16:05:34 +0100

    Add project files.

---
+++ Process-Release/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Process_Release
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.lbFolder = new System.Windows.Forms.Label();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.tsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lbNbrOfSubfolders = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(188, 80);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // lbFolder
+            // 
+            this.lbFolder.AutoSize = true;
+            this.lbFolder.Location = new System.Drawing.Point(54, 33);
+            this.lbFolder.Name = "lbFolder";
+            this.lbFolder.Size = new System.Drawing.Size(39, 13);
+            this.lbFolder.TabIndex = 1;
+            this.lbFolder.Text = "Folder:";
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Location = new System.Drawing.Point(95, 30);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.ReadOnly = true;
+            this.tbFolder.Size = new System.Drawing.Size(376, 20);
+            this.tbFolder.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(486, 28);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsProgress,
+            this.tsStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 131);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(612, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsProgress
+            // 
+            this.tsProgress.Name = "tsProgress";
+            this.tsProgress.Size = new System.Drawing.Size(200, 16);
+            // 
+            // tsStatus
+            // 
+            this.tsStatus.Name = "tsStatus";
+            this.tsStatus.Size = new System.Drawing.Size(29, 17);
+            this.tsStatus.Text = "Idle.";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(314, 80);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lbNbrOfSubfolders
+            // 
+            this.lbNbrOfSubfolders.AutoSize = true;
+            this.lbNbrOfSubfolders.Location = new System.Drawing.Point(237, 53);
+            this.lbNbrOfSubfolders.Name = "lbNbrOfSubfolders";
+            this.lbNbrOfSubfolders.Size = new System.Drawing.Size(0, 13);
+            this.lbNbrOfSubfolders.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 153);
+            this.Controls.Add(this.lbNbrOfSubfolders);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbFolder);
+            this.Controls.Add(this.lbFolder);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
+            this.Text = "Process-Release";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label lbFolder;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar tsProgress;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatus;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lbNbrOfSubfolders;
+    }
+}
+
---
+++ Process-Release/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Process_Release
+{
+    public partial class Form1 : Form
+    {
+        string[] extensions = new[] { ".m3u", ".nfo", ".sfv" }, lines, subFolders;
+        Thread backgroundThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbdRelease = new FolderBrowserDialog();
+            tsProgress.Value = 0;
+            try
+            {
+                fbdRelease.Description = "Choose Folder";
+                fbdRelease.SelectedPath = @"D:\Torrents";
+
+                if (fbdRelease.ShowDialog() == DialogResult.OK)
+                {
+                    tbFolder.Text = fbdRelease.SelectedPath;
+                    subFolders = Directory.GetDirectories(fbdRelease.SelectedPath);
+                    lbNbrOfSubfolders.Text = "Number of subfolders: " + subFolders.Length.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            fbdRelease.Dispose();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            setStatus("Processing...", true);
+            try
+            {
+                backgroundThread = new Thread(
+                new ThreadStart(() =>
+                {
+                    if (subFolders.Length == 0)
+                    {
+                        DirectoryInfo di = new DirectoryInfo(tbFolder.Text);
+                        FileInfo[] files = di.EnumerateFiles().Where(f => extensions.Contains(f.Extension.ToLower())).ToArray();
+
+                        foreach (FileInfo file in files)
+                        {
+                            ProcessFile(file.FullName);
+                        }
+                    }
+                    else
+                    {
+                        foreach (string folder in subFolders)
+                        {
+                            DirectoryInfo di = new DirectoryInfo(folder);
+                            FileInfo[] files = di.EnumerateFiles().Where(f => extensions.Contains(f.Extension.ToLower())).ToArray();
+                            // MessageBox.Show(files[0].ToString() + files[1].ToString() + files[2].ToString());
+
+                            foreach (FileInfo file in files)
+                            {
+                                ProcessFile(file.FullName);
+                            }
+                        }
+                    }
+                    this.BeginInvoke(new Action(() => { setStatus("Idle.", false); }));
+                }
+                ));
+                    backgroundThread.IsBackground = true;
+                    backgroundThread.Name = "Process";
+                    backgroundThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ProcessFile(string fileName)
+        {
+            using (StreamWriter w = File.AppendText(tbFolder.Text + @"\log.txt"))
+            {
+                lines = File.ReadAllLines(fileName, Encoding.GetEncoding(28591));
+                File.Delete(fileName);
+                File.WriteAllLines(fileName, lines, Encoding.GetEncoding(28591));
+                Log("- File " + fileName + " corrected.", w);
+            }
+        }
+
+        private static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\rLog Entry: ");
+            w.WriteLine("{0} {1} {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToLongTimeString(), logMessage);
+        }
+
+        private void setStatus(string status, bool disableUI)
+        {
+            tsStatus.Text = status;
+            tsStatus.Invalidate();
+
+            if (disableUI)
+            {
+                btnProcess.Enabled = false;
+                btnExit.Enabled = false;
+                btnBrowse.Enabled = false;
+                tsProgress.Style = ProgressBarStyle.Marquee;
+            }
+            else
+            {
+                btnProcess.Enabled = true;
+                btnExit.Enabled = true;
+                btnBrowse.Enabled = true;
+                tsProgress.Style = ProgressBarStyle.Blocks;
+            }
+            Application.DoEvents();
+        }
+    }
+}
